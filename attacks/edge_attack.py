fix: bugs in masking

attacks/edge_attack.py
@@ -95,36 +95,25 @@ def edge_attack_specific_nodes(data, epsilon, seed):
     poisoned_edges = set()
 
     # Use tqdm to show progress
-    with tqdm(total=epsilon, desc='Adding Poisoned Edges') as pbar:
-        while len(poisoned_edges) < epsilon:
-            n1 = np.random.choice(class1_indices.numpy())
-            n2 = np.random.choice(class2_indices.numpy())
-            edge = (min(n1, n2), max(n1, n2))
+    while len(poisoned_edges) < epsilon:
+        n1 = np.random.choice(class1_indices.numpy())
+        n2 = np.random.choice(class2_indices.numpy())
+        edge = (min(n1, n2), max(n1, n2))
 
-            if edge not in existing_edges and edge not in poisoned_edges:
-                to_arr.append(n1)
-                from_arr.append(n2)
-                poisoned_edges.add(edge)
-                poisoned_edges.add((n2, n1))
-                poisoned_nodes.add(n1)
-                poisoned_nodes.add(n2)
-                pbar.update(1)  # Update progress bar
+        if edge not in existing_edges and edge not in poisoned_edges:
+            to_arr.append(n1)
+            from_arr.append(n2)
+            poisoned_edges.add(edge)
+            poisoned_nodes.add(n1)
+            poisoned_nodes.add(n2)
 
     to_arr = torch.tensor(to_arr, dtype=torch.int64)
     from_arr = torch.tensor(from_arr, dtype=torch.int64)
     edge_index_to_add = torch.stack([to_arr, from_arr], dim=0)
     edge_index_to_add = utils.to_undirected(edge_index_to_add)
+
     data.poisoned_nodes= torch.tensor(list(poisoned_nodes), dtype=torch.long)
-    # Update edge_index
     augmented_edge = torch.cat([data.edge_index, edge_index_to_add], dim=1)
     data.edge_index = augmented_edge
-    nums= list(range(len(data.edge_index[0])-len(poisoned_edges), len(data.edge_index[0])))
+    nums= list(range(len(data.edge_index[0])-2*len(poisoned_edges), len(data.edge_index[0])))
     return data, torch.tensor(nums, dtype=torch.long)
-    # augmented_edge = utils.sort_edge_index(augmented_edge)
-
-    # # # Determine which added edges are poisoned
-    # augmented_edges_set = set((min(augmented_edge[0, i].item(), augmented_edge[1, i].item()),
-    #                            max(augmented_edge[0, i].item(), augmented_edge[1, i].item())) for i in range(augmented_edge.size(1)))
-
-    # added_edge_indices = [i for i, edge in enumerate(augmented_edges_set) if edge in poisoned_edges]
-

main.py
@@ -26,14 +26,11 @@ optimizer = torch.optim.Adam(clean_model.parameters(), lr=0.01, weight_decay=5e-
 clean_trainer = Trainer(clean_model, clean_data, optimizer, args.training_epochs)
 clean_trainer.train()
 
-
 print("==POISONING==")
 print(args.attack_type)
 if args.attack_type=="label":
-    print("A")
     poisoned_data, poisoned_indices = label_flip_attack(clean_data, args.df_size, args.random_seed)
 elif args.attack_type=="edge":
-    print("B")
     poisoned_data, poisoned_indices = edge_attack_specific_nodes(clean_data, args.df_size, args.random_seed)
 elif args.attack_type=="random":
     poisoned_data = copy.deepcopy(clean_data)
@@ -48,7 +45,10 @@ else:
 optimizer = torch.optim.Adam(poisoned_model.parameters(), lr=0.01, weight_decay=5e-4)
 poisoned_trainer = Trainer(poisoned_model, poisoned_data, optimizer, args.training_epochs)
 poisoned_trainer.train()
-utils.find_masks(poisoned_data, poisoned_indices, attack_type=args.attack_type)
+utils.find_masks(poisoned_data, poisoned_indices, args, attack_type=args.attack_type)
+
+acc= poisoned_trainer.all_class_acc()
+acc= clean_trainer.all_class_acc()
 exit(0)
 
 print("==UNLEARNING==")

trainers/base.py
@@ -31,48 +31,61 @@ class Trainer:
         print(f'Train Acc: {train_acc}, Misclassification: {msc_rate},  F1 Score: {f1}')
 
         return train_acc, msc_rate
-    
+
+    def all_class_acc(self):
+        classes = list(range(self.data.num_classes))
+        true_labels = self.true.to(device)
+        pred_labels = self.pred.to(device)
+        accs_clean = []
+
+        for clean_class in classes:
+            clean_indices = (true_labels == clean_class)
+            accs_clean.append(accuracy_score(true_labels[clean_indices].cpu(), pred_labels[clean_indices].cpu()))
+        print(f'class accuracies: {accs_clean}')
+        accs_clean = sum(accs_clean) / len(accs_clean)
+        return accs_clean
+
     def subset_acc(self, class1=None, class2=None):
         if class1 is None or class2 is None:
             class1 = self.class1
             class2 = self.class2
-        
+
         poisoned_classes = [class1, class2]
-        
+
         true_labels = self.true.to(device)
         pred_labels = self.pred.to(device)
-        
+
         clean_classes = []
         for i in range(self.data.num_classes):
             if i not in poisoned_classes:
                 clean_classes.append(i)
-        
+
         # calculate acc separately on poisoned and non-poisoned classes
         accs_poisoned = []
         accs_clean = []
-        
+
         for poisoned_class in poisoned_classes:
             poisoned_indices = (true_labels == poisoned_class)
             accs_poisoned.append(accuracy_score(true_labels[poisoned_indices].cpu(), pred_labels[poisoned_indices].cpu()))
-        
+
         for clean_class in clean_classes:
             clean_indices = (true_labels == clean_class)
             accs_clean.append(accuracy_score(true_labels[clean_indices].cpu(), pred_labels[clean_indices].cpu()))
-            
+
         print(f'Poisoned class: {class1} -> {class2} | Clean classes: {clean_classes}')
         print(f'Poisoned class acc: {accs_poisoned} | Clean class acc: {accs_clean}')
         # take average of the accs
         accs_poisoned = sum(accs_poisoned) / len(accs_poisoned)
         accs_clean = sum(accs_clean) / len(accs_clean)
-            
+
         return accs_poisoned, accs_clean
-        
-        
+
+
 
     def misclassification_rate(self, true_labels, pred_labels):
         if self.class1 is None or self.class2 is None:
             return 0
-            
+
         true_labels = true_labels.to(device)
         pred_labels = pred_labels.to(device)
         class1_to_class2 = ((true_labels == self.class1) & (pred_labels == self.class2)).sum().item()
@@ -84,7 +97,7 @@ class Trainer:
 
     def evaluate(self, is_dr=False):
         self.model.eval()
-        
+
         with torch.no_grad():
             if(is_dr):
                 z = F.log_softmax(self.model(self.data.x, self.data.edge_index[:, self.data.dr_mask]), dim=1)
@@ -95,8 +108,8 @@ class Trainer:
             dt_acc = accuracy_score(self.data.y[self.data.test_mask].cpu(), pred)
             dt_f1 = f1_score(self.data.y[self.data.test_mask].cpu(), pred, average='micro')
             msc_rate = self.misclassification_rate(self.data.y[self.data.test_mask].cpu(), pred)
-            
+
         self.true = self.data.y[self.data.test_mask].cpu()
         self.pred = pred
-                        
+
         return dt_acc, msc_rate, dt_f1

trainers/scrub.py
@@ -1,4 +1,4 @@
-import torch, torchmetrics, tqdm, copy, time
+import torch, tqdm, copy, time
 import numpy as np
 from os import makedirs
 from os.path import exists
@@ -33,7 +33,7 @@ class LinearLR(_LRScheduler):
         optimizer (Optimizer): Wrapped optimizer.
         T (int): Total number of training epochs or iterations.
         last_epoch (int): The index of last epoch or iteration. Default: -1.
-        
+
     .. _Budgeted Training\: Rethinking Deep Neural Network Training Under
     Resource Constraints:
         https://arxiv.org/abs/1905.04753
@@ -53,7 +53,7 @@ class LinearLR(_LRScheduler):
 
     def _get_closed_form_lr(self):
         return self.get_lr()
-    
+
 class ScrubTrainer(Trainer):
     def __init__(self, model, poisoned_dataset, optimizer, opt):
         super().__init__(model, poisoned_dataset, optimizer)
@@ -65,7 +65,7 @@ class ScrubTrainer(Trainer):
         self.poisoned_dataset = poisoned_dataset
 
 
-        self.scheduler = LinearLR(self.optimizer, T=self.opt.unlearn_iters*1.25, warmup_epochs=self.opt.unlearn_iters//100) # Spend 1% time in warmup, and stop 66% of the way through training 
+        self.scheduler = LinearLR(self.optimizer, T=self.opt.unlearn_iters*1.25, warmup_epochs=self.opt.unlearn_iters//100) # Spend 1% time in warmup, and stop 66% of the way through training
         self.og_model = copy.deepcopy(model)
         self.og_model.eval()
         opt.unlearn_iters = opt.unlearn_iters
@@ -89,7 +89,7 @@ class ScrubTrainer(Trainer):
         return
 
     def forward_pass(self, data, mask):
-        
+
         output = self.model(data.x, data.edge_index)
 
         with torch.no_grad():
@@ -97,14 +97,14 @@ class ScrubTrainer(Trainer):
 
         loss = F.cross_entropy(output[mask], data.y[mask])
         loss += self.opt.scrubAlpha * distill_kl_loss(output[mask], logit_t[mask], self.opt.kd_T)
-        
+
         if self.maximize:
             loss = -loss
 
         return loss
 
     def unlearn_nc(self, dataset, train_mask, forget_mask):
-        
+
         self.maximize=False
         while self.curr_step < self.opt.unlearn_iters:
             if self.curr_step < self.opt.msteps:
@@ -114,7 +114,7 @@ class ScrubTrainer(Trainer):
             self.maximize=False
             self.train_one_epoch(data=dataset, mask=train_mask)
         return
-    
+
     # scrub for label flipping
     def unlearn_nc_lf(self):
         attacked_indices = self.poisoned_dataset.attacked_idx
@@ -131,10 +131,10 @@ class ScrubTrainer(Trainer):
         train_acc, msc_rate, f1 = self.evaluate()
         print(f'Test Acc: {train_acc}, Misclassification: {msc_rate},  F1 Score: {f1}')
         return train_acc, msc_rate, 0
-    
+
     def train(self):
         return self.unlearn_nc_lf()
-    
+
     def get_save_prefix(self):
         self.unlearn_file_prefix = self.opt.pretrain_file_prefix+'/'+str(self.opt.deletion_size)+'_'+self.opt.unlearn_method+'_'+self.opt.exp_name
         self.unlearn_file_prefix += '_'+str(self.opt.unlearn_iters)+'_'+str(self.opt.k)