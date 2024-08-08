41a002891f53ddfdd9339ec098af92ffb1af9f45
Debangan-MishraIIIT <debangan.mishra@students.iiit.ac.in>
2024-08-08 12:04:31 +0530

    feat: HIN sampling

--- trainers/contrast.py
+++ trainers/contrast.py
@@ -1,14 +1,14 @@
 import os, math
 import copy
 import time
-
+import scipy.sparse as sp
 # import wandb
 import numpy as np
 from tqdm import tqdm, trange
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.utils import negative_sampling, k_hop_subgraph
+from torch_geometric.utils import negative_sampling, k_hop_subgraph, to_scipy_sparse_matrix
 from torch_geometric.loader import GraphSAINTRandomWalkSampler
 
 from .base import Trainer
@@ -22,7 +22,7 @@ def time_it(func):
         result = func(*args, **kwargs)
         end_time = time.time()
         elapsed_time = end_time - start_time
-        print(f"Function '{func.__name__}' took {elapsed_time:.4f} seconds to execute.")
+        #print(f"Function '{func.__name__}' took {elapsed_time:.4f} seconds to execute.")
         return result
 
     return wrapper
@@ -36,7 +36,110 @@ class ContrastiveUnlearnTrainer(Trainer):
         self.embeddings = None
         self.criterion = torch.nn.CrossEntropyLoss()
 
+    # def get_sample_points(self):
+    #     # get the k-hop subgraph of attacked nodes, and add all the nodes in the subgraph to the sample_mask
+    #     sample_mask = torch.zeros(self.data.num_nodes, dtype=torch.bool)
+    #     for idx in self.attacked_idx:
+    #         idx_int = int(idx)
+    #         subset, _, _, _ = k_hop_subgraph(
+    #             idx_int, self.args.k_hop, self.data.edge_index
+    #         )
+    #         sample_mask[subset] = True
+
+    #     #print(f"Number of nodes in the sampling: {sample_mask.sum().item()}")
+
+    #     eps = self.args.contrastive_eps
+    #     # add eps fraction of non-neighbor training nodes to the sample_mask
+    #     num_to_add = int(eps * self.data.train_mask.sum().item())
+    #     # TODO: check if the non-neighbors are being sampled correctly
+
+    #     self.data.sample_mask = sample_mask
+
+    def propagate(self, features, k, adj_norm):
+        feature_list = []
+
+        features= features.cpu()
+        adj_norm= adj_norm.cpu()
+        feature_list.append(features)
+
+        for i in range(k):
+            feature_list.append(torch.spmm(adj_norm, feature_list[-1]))
+        return feature_list[-1]
+
+
+    def reverse_features(self, features):
+        reverse_features = features.clone()
+        for idx in self.attacked_idx:
+            reverse_features[idx] = 1-reverse_features[idx]
+
+        return reverse_features
+
+    def sparse_mx_to_torch_sparse_tensor(self, sparse_mx):
+        """Convert a scipy sparse matrix to a torch sparse tensor."""
+        sparse_mx = sparse_mx.tocoo().astype(np.float32)
+        indices = torch.from_numpy(
+            np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+        values = torch.from_numpy(sparse_mx.data)
+        shape = torch.Size(sparse_mx.shape)
+        return torch.sparse_coo_tensor(indices, values, shape, dtype=torch.float)
+
+    def normalize_adj(self, adj, r=0.5):
+        adj = adj + sp.eye(adj.shape[0])
+        degrees = np.array(adj.sum(1))
+        r_inv_sqrt_left = np.power(degrees, r - 1).flatten()
+        r_inv_sqrt_left[np.isinf(r_inv_sqrt_left)] = 0.
+        r_mat_inv_sqrt_left = sp.diags(r_inv_sqrt_left)
+
+        r_inv_sqrt_right = np.power(degrees, -r).flatten()
+        r_inv_sqrt_right[np.isinf(r_inv_sqrt_right)] = 0.
+        r_mat_inv_sqrt_right = sp.diags(r_inv_sqrt_right)
+
+        adj_normalized = adj.dot(r_mat_inv_sqrt_left).transpose().dot(r_mat_inv_sqrt_right)
+        return adj_normalized
+
+    #MEGU HIN sampling
     def get_sample_points(self):
+        self.adj = self.sparse_mx_to_torch_sparse_tensor(self.normalize_adj(to_scipy_sparse_matrix(self.data.edge_index)))
+        temp_features = self.data.x.clone()
+        pfeatures = self.propagate(temp_features, self.args.k_hop, self.adj)
+        reverse_feature = self.reverse_features(temp_features)
+        re_pfeatures = self.propagate(reverse_feature, self.args.k_hop, self.adj)
+
+        cos = nn.CosineSimilarity()
+        sim = cos(pfeatures, re_pfeatures)
+
+        alpha = 0.1
+        gamma = 0.1
+        max_val = 0.
+        while True:
+            influence_nodes_with_unlearning_nodes = torch.nonzero(sim <= alpha).flatten().cpu()
+            if len(influence_nodes_with_unlearning_nodes.view(-1)) > 0:
+                temp_max = torch.max(sim[influence_nodes_with_unlearning_nodes])
+            else:
+                alpha = alpha + gamma
+                continue
+
+            if temp_max == max_val:
+                break
+
+            max_val = temp_max
+            alpha = alpha + gamma
+
+        neighborkhop, _, _, two_hop_mask = k_hop_subgraph(
+            torch.tensor(self.attacked_idx),
+            self.args.k_hop,
+            self.data.edge_index,
+            num_nodes=self.data.num_nodes)
+
+        neighborkhop = neighborkhop[~np.isin(neighborkhop.cpu(), self.attacked_idx)]
+        neighbor_nodes = []
+        for idx in influence_nodes_with_unlearning_nodes:
+            if idx in neighborkhop and idx not in self.attacked_idx:
+                neighbor_nodes.append(idx.item())
+
+        self.data.sample_mask = torch.from_numpy(np.isin(np.arange(self.data.num_nodes), neighbor_nodes))
+
+
         # get the k-hop subgraph of attacked nodes, and add all the nodes in the subgraph to the sample_mask
         sample_mask = torch.zeros(self.data.num_nodes, dtype=torch.bool)
         for idx in self.attacked_idx:
@@ -45,9 +148,9 @@ class ContrastiveUnlearnTrainer(Trainer):
                 idx_int, self.args.k_hop, self.data.edge_index
             )
             sample_mask[subset] = True
-        
-        print(f"Number of nodes in the sampling: {sample_mask.sum().item()}")        
-        
+
+        #print(f"Number of nodes in the sampling: {sample_mask.sum().item()}")
+
         eps = self.args.contrastive_eps
         # add eps fraction of non-neighbor training nodes to the sample_mask
         num_to_add = int(eps * self.data.train_mask.sum().item())
@@ -118,7 +221,7 @@ class ContrastiveUnlearnTrainer(Trainer):
     def get_distances_batch(self, batch_size=64):
         st = time.time()
         self.embeddings = self.model(self.data.x, self.data.edge_index)
-        print(f"Time taken to get embeddings: {time.time() - st}")
+        #print(f"Time taken to get embeddings: {time.time() - st}")
 
         num_masks = len(self.data.train_mask)
         pos_dist = torch.zeros(num_masks)
@@ -129,8 +232,8 @@ class ContrastiveUnlearnTrainer(Trainer):
 
         sample_indices = torch.where(self.data.sample_mask)[0]
         num_samples = len(sample_indices)
-        
-        print(f"Number of samples: {num_samples}")
+
+        #print(f"Number of samples: {num_samples}")
 
         attacked_set = set(self.attacked_idx.tolist())
 
@@ -173,8 +276,8 @@ class ContrastiveUnlearnTrainer(Trainer):
             pos_dist[batch_indices] = batch_pos_dist.to(pos_dist.device)
             neg_dist[batch_indices] = batch_neg_dist.to(neg_dist.device)
 
-        print(f"Average time taken to calculate distances: {calc_time/num_samples}")
-        print(f"Average time taken to get distances: {(time.time() - st)/num_samples}")
+        #print(f"Average time taken to calculate distances: {calc_time/num_samples}")
+        #print(f"Average time taken to get distances: {(time.time() - st)/num_samples}")
 
         return pos_dist, neg_dist
 
